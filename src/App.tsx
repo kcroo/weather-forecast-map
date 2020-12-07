f2843bc511ba94493544e602af1536c5547c7f3f
Kirsten Corrao <kirstencorrao@gmail.com>
2020-12-07 15:06:56 -0800

    displays two markers on map

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
@@ -14,19 +14,32 @@ let DefaultIcon = L.icon({
 });
 L.Marker.prototype.options.icon = DefaultIcon;
 
+interface Location {
+  name: string,
+  latitude: number,
+  longitude: number
+};
+
 function App() {
+  const [locations, setLocations] = useState<Location[]>([
+    {name: 'Walla Walla', latitude: 46.07123, longitude: -118.29352},
+    {name: 'Tollgate', latitude: 45.781037, longitude: -118.09154}
+  ]);
+
   return (
     <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[46.07123, -118.29352]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {locations.map(loc => { 
+        return (
+          <Marker position={L.latLng(loc.latitude, loc.longitude)}>
+            <Popup>{loc.name}</Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
     </div>
   );
