0fb9a222b61ef63a28b25baf27aeee920cee04cc
Kirsten Corrao <kirstencorrao@gmail.com>
2020-12-07 15:42:05 -0800

    centers to locations in array

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+// render react components from array of objects: https://stackoverflow.com/questions/32157286/rendering-react-components-from-array-of-objects
+// get bounds from array of points: https://stackoverflow.com/questions/41926938/how-to-get-the-bounds-of-a-collection-of-markers-with-react-leaflet
+
 import React, { useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet'
+import L, { latLngBounds } from 'leaflet'
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
@@ -26,16 +29,24 @@ function App() {
     {name: 'Tollgate', latitude: 45.781037, longitude: -118.09154}
   ]);
 
+  // set bounds of the map, based off of locations above
+  // make this state later if locations array can change & make useEffect to update it when locations change
+  const bounds = latLngBounds([locations[0].latitude, locations[0].longitude], [locations[0].latitude, locations[0].longitude]);
+  for (let location of locations) {
+    console.log(`extending bounds: ${location.latitude}, ${location.longitude}`)
+    bounds.extend([location.latitude, location.longitude]);
+  }
+
   return (
     <div>
-      <MapContainer center={[46.07123, -118.29352]} zoom={13}>
+      <MapContainer bounds={bounds}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {locations.map(loc => { 
         return (
-          <Marker position={L.latLng(loc.latitude, loc.longitude)}>
+          <Marker key={loc.name} position={L.latLng(loc.latitude, loc.longitude)}>
             <Popup>{loc.name}</Popup>
           </Marker>
         );
