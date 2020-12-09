0928b466e4c34adf3ad72d18ed2117cd7f73871f
Kirsten Corrao <kirstencorrao@gmail.com>
2020-12-09 15:36:14 -0800

    shows month and date instead of unix time

--- src/components/DayForecast.tsx
+++ src/components/DayForecast.tsx
@@ -9,10 +9,15 @@ function kelvinToFahrenheit(temp: number): number {
   return Math.round(1.8 * (temp - 273) + 32);
 }
 
+function unixTimeToMonthDate(unixTime: string): string {
+  const date = new Date(parseInt(unixTime) * 1000);
+  return `${date.getMonth()}/${date.getDate()}`;
+}
+
 function DayForecast({dayForecast}: Props) {
   return (
     <div>
-      <h3>{dayForecast.date}</h3>
+      <h3>{unixTimeToMonthDate(dayForecast.date)}</h3>
       <p>{kelvinToFahrenheit(dayForecast.highTemp)}</p>
       <p>{kelvinToFahrenheit(dayForecast.lowTemp)}</p>
     </div>
